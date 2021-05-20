d11f4184019ac2af4b239e27e76c68908e3f32aa
VCD <VeryCrazyDog@gmail.com>
2021-05-20 21:00:47 +0800

    Refactor test cases

--- src/test/delay.test.ts
+++ src/test/delay.test.ts
@@ -5,20 +5,20 @@ import test, { ExecutionContext } from 'ava'
 import { delay } from '../index'
 
 // Private functions
-function assertExecTimeAround (t: ExecutionContext, startTime: [number, number], execTime: number): void {
+function assertExecTimeAround (t: ExecutionContext, execTime: number, startTime: bigint, endTime: bigint): void {
   const ALLOW_RANGE_LOWER = 10
   const ALLOW_RANGE_UPPER = 25
-  const [, endNs] = process.hrtime(startTime)
-  const endMs = endNs / 1000000
+  const diffNs = endTime - startTime
+  const diffMs = diffNs / BigInt(1000000)
   const lowerAllowed = execTime - ALLOW_RANGE_LOWER
   const upperAllowed = execTime + ALLOW_RANGE_UPPER
   t.true(
-    endMs >= lowerAllowed,
-    `Expect actual execution time ${endMs} greater than or equal to ${lowerAllowed}`
+    diffMs >= lowerAllowed,
+    `Expect actual execution time ${diffMs} greater than or equal to ${lowerAllowed}`
   )
   t.true(
-    endMs <= upperAllowed,
-    `Expect actual execution time ${endMs} less than or equal to ${upperAllowed}`
+    diffMs <= upperAllowed,
+    `Expect actual execution time ${diffMs} less than or equal to ${upperAllowed}`
   )
 }
 
@@ -30,9 +30,10 @@ test('should resolved to undefined when no value is passed', async (t) => {
 
 test('should delay execution', async (t) => {
   const DELAY = 200
-  const startTime = process.hrtime()
+  const startTime = process.hrtime.bigint()
   await delay(DELAY)
-  assertExecTimeAround(t, startTime, DELAY)
+  const endTime = process.hrtime.bigint()
+  assertExecTimeAround(t, DELAY, startTime, endTime)
 })
 
 test('should resolved to null when null is passed', async (t) => {
@@ -64,14 +65,16 @@ test('should resolved to KeyValuePair when promise of KeyValuePair is passed', a
 
 test('should delay return after promise resolved', async (t) => {
   const input = 'hello'
-  const startTime = process.hrtime()
+  const startTime = process.hrtime.bigint()
   const delayJob = new Promise<string>(resolve => {
     setTimeout(() => {
-      assertExecTimeAround(t, startTime, 100)
+      const endTime = process.hrtime.bigint()
+      assertExecTimeAround(t, 100, startTime, endTime)
       resolve(input)
     }, 100)
   })
   const output = await delay<string>(200, delayJob)
-  assertExecTimeAround(t, startTime, 300)
+  const endTime = process.hrtime.bigint()
+  assertExecTimeAround(t, 300, startTime, endTime)
   t.is(output, input)
 })
