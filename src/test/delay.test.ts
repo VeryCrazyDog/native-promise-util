caa3cedecedca9972c9e2a775968d86a6a6ebe7d
VCD <VeryCrazyDog@gmail.com>
2021-05-21 20:53:42 +0800

    Add retry on execution time sensitive test cases

--- src/test/delay.test.ts
+++ src/test/delay.test.ts
@@ -1,10 +1,24 @@
 // Import 3rd party modules
-import test, { ExecutionContext } from 'ava'
+import test, { EitherMacro, ExecutionContext } from 'ava'
 
 // Import module to be tested
 import { delay } from '../index'
 
 // Private functions
+async function tryRepeat<C> (t: ExecutionContext<C>, fn: EitherMacro<undefined[], C>, attempt: number): Promise<void> {
+  for (let i = 1; i <= attempt; i++) {
+    const tryResult = await t.try(fn)
+    if (tryResult.passed) {
+      tryResult.commit()
+      break
+    } else if (i !== attempt) {
+      tryResult.discard()
+    } else {
+      tryResult.commit()
+    }
+  }
+}
+
 function assertExecTimeAround (t: ExecutionContext, execTime: number, startTime: bigint, endTime: bigint): void {
   const ALLOW_RANGE_LOWER = 10
   const ALLOW_RANGE_UPPER = 25
@@ -23,32 +37,34 @@ function assertExecTimeAround (t: ExecutionContext, execTime: number, startTime:
 }
 
 // Test cases
-test('should resolved to undefined when no value is passed', async (t) => {
+test('should resolved to undefined when no value is passed', async t => {
   const output = await delay(1)
   t.is(output, undefined)
 })
 
-test('should delay execution', async (t) => {
+test('should delay execution', async t => {
   const DELAY = 200
-  const startTime = process.hrtime.bigint()
-  await delay(DELAY)
-  const endTime = process.hrtime.bigint()
-  assertExecTimeAround(t, DELAY, startTime, endTime)
+  await tryRepeat(t, async tt => {
+    const startTime = process.hrtime.bigint()
+    await delay(DELAY)
+    const endTime = process.hrtime.bigint()
+    assertExecTimeAround(tt, DELAY, startTime, endTime)
+  }, 3)
 })
 
-test('should resolved to null when null is passed', async (t) => {
+test('should resolved to null when null is passed', async t => {
   const input = null
   const output = await delay<null>(1, input)
   t.is(output, input)
 })
 
-test('should resolved to \'hello\' when \'hello\' is passed', async (t) => {
+test('should resolved to \'hello\' when \'hello\' is passed', async t => {
   const input = 'hello'
   const output = await delay<string>(1, input)
   t.is(output, input)
 })
 
-test('should resolved to KeyValuePair when promise of KeyValuePair is passed', async (t) => {
+test('should resolved to KeyValuePair when promise of KeyValuePair is passed', async t => {
   interface KeyValuePair {
     id: number
     key: string
@@ -63,18 +79,20 @@ test('should resolved to KeyValuePair when promise of KeyValuePair is passed', a
   })
 })
 
-test('should delay return after promise resolved', async (t) => {
-  const input = 'hello'
-  const startTime = process.hrtime.bigint()
-  const delayJob = new Promise<string>(resolve => {
-    setTimeout(() => {
-      const endTime = process.hrtime.bigint()
-      assertExecTimeAround(t, 100, startTime, endTime)
-      resolve(input)
-    }, 100)
-  })
-  const output = await delay<string>(200, delayJob)
-  const endTime = process.hrtime.bigint()
-  assertExecTimeAround(t, 300, startTime, endTime)
-  t.is(output, input)
+test('should delay return after promise resolved', async t => {
+  await tryRepeat(t, async tt => {
+    const input = 'hello'
+    const startTime = process.hrtime.bigint()
+    const delayJob = new Promise<string>(resolve => {
+      setTimeout(() => {
+        const endTime = process.hrtime.bigint()
+        assertExecTimeAround(tt, 100, startTime, endTime)
+        resolve(input)
+      }, 100)
+    })
+    const output = await delay<string>(200, delayJob)
+    const endTime = process.hrtime.bigint()
+    assertExecTimeAround(tt, 300, startTime, endTime)
+    tt.is(output, input)
+  }, 3)
 })
